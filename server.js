0e20182f6585604ba076ec9e13a97c88b3fcbc9f
gabe <gabekamalakis@proton.com>
2020-04-27 15:52:23 -0400

    Created barebones server with Express

---
+++ server.js
@@ -0,0 +1,16 @@
+// Importing required libraries
+const express = require('express');
+
+const application = express();
+
+
+// Port selection
+const port = process.env.PORT || 3000;
+
+// JSON data Features
+
+// Wiring up server to use Public folder
+application.use(express.static('public'));
+
+
+application.listen(port, () => console.log(`LitterLogger listening on port ${port}!`));
