eb7853d89e694f065c875a96a5f280278dc6755d
Andrew Garcia <60405634+pagarcia377@users.noreply.github.com>
2020-04-29 16:15:21 -0400

    Format whitespace

--- server.js
+++ server.js
@@ -3,14 +3,14 @@
 import express from "express";
 const application = express();
 
-
 // Port selection
 const port = process.env.PORT || 3000;
 
 // JSON data Features
 
 // Wiring up server to use Public folder
-application.use(express.static('public'));
+application.use(express.static("public"));
 
-
-application.listen(port, () => console.log(`LitterLogger listening on port ${port}!`));
+application.listen(port, () =>
+  console.log(`LitterLogger listening on port ${port}!`)
+);
