99f506057661a7380b74665902ca4b682665195e
gabe <gabekamalakis@proton.com>
2020-05-01 23:16:08 -0400

    Added non-functional SQL functions

--- server.js
+++ server.js
@@ -4,20 +4,30 @@ import express from "express";
 //const fetch = require("node-fetch");
 import fetch from "node-fetch";
 
-const application = express();
+// Importing SQLite
+import sqlite3 from "sqlite3";
+
+const dbSettings = {
+	filename: './tmp/litterdata.db',
+	driver: sqlite3.Database
+};
+
+
+
+const app = express();
 
 // Port selection
 const port = process.env.PORT || 3000;
 
 // Wiring up server to use Public folder
-application.use(express.static("public"));
+app.use(express.static("public"));
 
-application.listen(port, () =>
+app.listen(port, () =>
   console.log(`LitterLogger server listening on port ${port}!`)
 );
 
 // JSON data Features
-application.get("/api", (req, res) => {
+app.get("/api", (req, res) => {
   processDataForFrontEnd(req, res);
 });
 
@@ -29,8 +39,10 @@ function processDataForFrontEnd(req, res) {
   fetch(baseURL)
     .then((response) => response.json())
     .then((data) => {
+
       console.log("LitterTrak data", data);
       const dataPoints = [];
+
       data.forEach((r) => {
         dataPoints.push({
           lat: r.geocoded_column.longitude,
@@ -38,6 +50,7 @@ function processDataForFrontEnd(req, res) {
           total_bags: r.total_bags_litter,
         })
       });
+
       console.log("Filter to lat, long, and total bags", dataPoints);
       res.send({ dataPoints }); // Return data to front end
     })
