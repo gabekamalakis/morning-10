8a0f1d0e5b608c3b14d574e340f2b8e0937ab03c
pagarcia377 <60405634+pagarcia377@users.noreply.github.com>
2020-05-01 21:49:41 -0400

    Use forEach

--- server.js
+++ server.js
@@ -2,13 +2,12 @@
 //const express = require("express");
 import express from "express";
 //const fetch = require("node-fetch");
-import fetch from "node-fetch"
+import fetch from "node-fetch";
+
 const application = express();
 
 // Port selection
 const port = process.env.PORT || 3000;
-
-// JSON data Features
 
 // Wiring up server to use Public folder
 application.use(express.static("public"));
@@ -17,6 +16,7 @@ application.listen(port, () =>
   console.log(`LitterLogger listening on port ${port}!`)
 );
 
+// JSON data Features
 application.get("/api", (req, res) => {
   processDataForFrontEnd(req, res);
 });
@@ -27,26 +27,18 @@ function processDataForFrontEnd(req, res) {
 
   //Fetch API Call
   fetch(baseURL)
-    .then((r) => r.json())
+    .then((response) => response.json())
     .then((data) => {
       console.log("LitterTrak data", data);
       const dataPoints = [];
-      // for (key in data) {
-      //   dataPoints.push({
-      //     lat: data[key].geocoded_column[key].longitude,
-      //     lng: data[key].geocoded_column[].latitude,
-      //     total_bags: data[key].total_bags_litter,
-      //   });
-      // }
-      for (let i=0; i<data.length; i++) {
+      data.forEach((r) => {
         dataPoints.push({
-          lat: data[i].geocoded_column.longitude,
-          lng: data[i].geocoded_column.latitude,
-          total_bags: data[i].total_bags_litter,
-        });
-      }
+          lat: r.geocoded_column.longitude,
+          lng: r.geocoded_column.latitude,
+          total_bags: r.total_bags_litter,
+        })
+      });
       console.log("Filter to lat, long, and total bags", dataPoints);
-
       res.send({ dataPoints }); // Return data to front end
     })
     .catch((err) => {
