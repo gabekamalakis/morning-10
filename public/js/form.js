e94030aff083914c87135a006346c6255aaef247
pagarcia377 <60405634+pagarcia377@users.noreply.github.com>
2020-05-03 18:10:17 -0400

    Add datetime picker

---
+++ public/js/form.js
@@ -0,0 +1,21 @@
+// From https://demo.creativebulma.net/components/calendar/v6//#integration
+// Initialize all input of date type.
+const options = {};
+const calendars = bulmaCalendar.attach('[type="datetime"]', options);
+
+// Loop on each calendar initialized
+calendars.forEach(calendar => {
+	// Add listener to date:selected event
+	calendar.on('date:selected', date => {
+		console.log(date);
+	});
+});
+
+// To access to bulmaCalendar instance of an element
+const element = document.querySelector('#my-element');
+if (element) {
+	// bulmaCalendar instance is available as element.bulmaCalendar
+	element.bulmaCalendar.on('select', datepicker => {
+		console.log(datepicker.data.value());
+	});
+}
