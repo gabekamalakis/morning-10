3e3fd17dd502bfb062a65cd90678025ef81e2b68
gabekamalakis <60404182+gabekamalakis@users.noreply.github.com>
2020-05-06 14:56:57 -0400

    Revert "Made SQLite chooch"
    This reverts commit 1cff50e4d35198a5e6e37d8fabcb47be70de49fb.

--- server/sqlCommands.js
+++ server/sqlCommands.js
@@ -1,26 +1,20 @@
-// import sqlite3 from 'sqlite3';
-import  { open } from 'sqlite';
+import sqlite3 from 'sqlite3';
+import { open } from 'sqlite';
 
 async function createTables(dbSettings) {
 	console.log('Initializing Database');
 
 	const db = await open(dbSettings);
-	await db.exec('CREATE TABLE IF NOT EXISTS user (name, email, phoneNumber, trashType, weight, numBags, locations)');
+	await db.exec('CREATE TABLE IF NOT EXISTS user (name, email, phoneNumber)');
+	await db.exec('CREATE TABLE IF NOT EXISTS trash (numBags)');
+
 
 	// This is a check to see if it's working
-	// const result  = await db.each('SHOW TABLES;', (err) => {
-	// 	console.log('createTables', err);
-	// 	});
-	const result = "Success";
+	const result  = await db.each('SHOW TABLES;', (err) => {
+		console.log('createTables', err);
+	});
 
 	return result;
-}
-
-async function addPickup(dbSettings, name, email, phoneNumber, trashType, weight, numBags) {
-
-	const db = await open(dbSettings);
-	await db.exec(`INSERT INTO user VALUES ("$name", "$email", "$phoneNumber", "$trashType", "$weight", "$numBags")`)
-
 }
 
 export default createTables;
